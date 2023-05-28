a7067eca29007f9b4c5f8a4e7104a0269599e3c6
Grace <grace.thompson97@gmail.com>
2023-05-28 08:40:28 -0700

    pick ability scores by class

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"sort"
+	"strings"
 
 	"github.com/sgade/randomorg"
 )
@@ -45,11 +47,183 @@ func generateAbilityScores() []int {
 			abilityScores[i] += abilityRolls[i][j]
 		}
 	}
+	sort.Ints(abilityScores)
+	return abilityScores
+}
+
+func getCharInfo() (string, string, string, string) {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("What is your character's name? ")
+	charName, _ := reader.ReadString('\n')
+	charName = strings.TrimSuffix(charName, "\n")
+	charName = strings.Title(charName)
+
+	fmt.Print("What is your character's race? ")
+	charRace, _ := reader.ReadString('\n')
+	charRace = strings.TrimSuffix(charRace, "\n")
+	charRace = strings.Title(charRace)
+
+	fmt.Print("What is your character's subrace? ")
+	charSubRace, _ := reader.ReadString('\n')
+	charSubRace = strings.TrimSuffix(charSubRace, "\n")
+	charSubRace = strings.Title(charSubRace)
+
+	fmt.Print("What is your character's class? ")
+	charClass, _ := reader.ReadString('\n')
+	charClass = strings.TrimSuffix(charClass, "\n")
+	charClass = strings.Title(charClass)
+
+	return charName, charRace, charSubRace, charClass
+}
+
+func finalizeScores() []int {
+	_, _, _, charClass := getCharInfo()
+	rolledScores := generateAbilityScores()
+	abilityScores := []int{0, 0, 0, 0, 0, 0}
+	if charClass == "Barbarian" {
+		strength := rolledScores[len(rolledScores)-1]
+		constitution := rolledScores[len(rolledScores)-2]
+		wisdom := rolledScores[len(rolledScores)-3]
+		dexterity := rolledScores[len(rolledScores)-4]
+		charisma := rolledScores[len(rolledScores)-5]
+		intelligence := rolledScores[len(rolledScores)-6]
+		abilityScores := []int{strength, dexterity, constitution, intelligence, wisdom, charisma}
+		return abilityScores
+
+	} else if charClass == "Bard" {
+		charisma := rolledScores[len(rolledScores)-1]
+		dexterity := rolledScores[len(rolledScores)-2]
+		constitution := rolledScores[len(rolledScores)-3]
+		intelligence := rolledScores[len(rolledScores)-4]
+		wisdom := rolledScores[len(rolledScores)-5]
+		strength := rolledScores[len(rolledScores)-6]
+		abilityScores := []int{strength, dexterity, constitution, intelligence, wisdom, charisma}
+		return abilityScores
+
+	} else if charClass == "Cleric" {
+		wisdom := rolledScores[len(rolledScores)-1]
+		strength := rolledScores[len(rolledScores)-2]
+		constitution := rolledScores[len(rolledScores)-3]
+		dexterity := rolledScores[len(rolledScores)-4]
+		intelligence := rolledScores[len(rolledScores)-5]
+		charisma := rolledScores[len(rolledScores)-6]
+		abilityScores := []int{strength, dexterity, constitution, intelligence, wisdom, charisma}
+		return abilityScores
+
+	} else if charClass == "Druid" {
+		wisdom := rolledScores[len(rolledScores)-1]
+		constitution := rolledScores[len(rolledScores)-2]
+		dexterity := rolledScores[len(rolledScores)-3]
+		charisma := rolledScores[len(rolledScores)-4]
+		intelligence := rolledScores[len(rolledScores)-5]
+		strength := rolledScores[len(rolledScores)-6]
+		abilityScores := []int{strength, dexterity, constitution, intelligence, wisdom, charisma}
+		return abilityScores
+
+	} else if charClass == "Fighter" {
+		strength := rolledScores[len(rolledScores)-1]
+		constitution := rolledScores[len(rolledScores)-2]
+		dexterity := rolledScores[len(rolledScores)-3]
+		wisdom := rolledScores[len(rolledScores)-4]
+		charisma := rolledScores[len(rolledScores)-5]
+		intelligence := rolledScores[len(rolledScores)-6]
+		abilityScores := []int{strength, dexterity, constitution, intelligence, wisdom, charisma}
+		return abilityScores
+
+	} else if charClass == "Monk" {
+		dexterity := rolledScores[len(rolledScores)-1]
+		wisdom := rolledScores[len(rolledScores)-2]
+		constitution := rolledScores[len(rolledScores)-3]
+		intelligence := rolledScores[len(rolledScores)-4]
+		strength := rolledScores[len(rolledScores)-5]
+		charisma := rolledScores[len(rolledScores)-6]
+		abilityScores := []int{strength, dexterity, constitution, intelligence, wisdom, charisma}
+		return abilityScores
+
+	} else if charClass == "Paladin" {
+		strength := rolledScores[len(rolledScores)-1]
+		charisma := rolledScores[len(rolledScores)-2]
+		constitution := rolledScores[len(rolledScores)-3]
+		wisdom := rolledScores[len(rolledScores)-4]
+		intelligence := rolledScores[len(rolledScores)-5]
+		dexterity := rolledScores[len(rolledScores)-6]
+		abilityScores := []int{strength, dexterity, constitution, intelligence, wisdom, charisma}
+		return abilityScores
+
+	} else if charClass == "Paladin" {
+		strength := rolledScores[len(rolledScores)-1]
+		charisma := rolledScores[len(rolledScores)-2]
+		constitution := rolledScores[len(rolledScores)-3]
+		wisdom := rolledScores[len(rolledScores)-4]
+		intelligence := rolledScores[len(rolledScores)-5]
+		dexterity := rolledScores[len(rolledScores)-6]
+		abilityScores := []int{strength, dexterity, constitution, intelligence, wisdom, charisma}
+		return abilityScores
+
+	} else if charClass == "Ranger" {
+		dexterity := rolledScores[len(rolledScores)-1]
+		wisdom := rolledScores[len(rolledScores)-2]
+		strength := rolledScores[len(rolledScores)-3]
+		intelligence := rolledScores[len(rolledScores)-4]
+		constitution := rolledScores[len(rolledScores)-5]
+		charisma := rolledScores[len(rolledScores)-6]
+		abilityScores := []int{strength, dexterity, constitution, intelligence, wisdom, charisma}
+		return abilityScores
+
+	} else if charClass == "Rogue" {
+		dexterity := rolledScores[len(rolledScores)-1]
+		charisma := rolledScores[len(rolledScores)-2]
+		intelligence := rolledScores[len(rolledScores)-3]
+		wisdom := rolledScores[len(rolledScores)-4]
+		constitution := rolledScores[len(rolledScores)-5]
+		strength := rolledScores[len(rolledScores)-6]
+		abilityScores := []int{strength, dexterity, constitution, intelligence, wisdom, charisma}
+		return abilityScores
+
+	} else if charClass == "Sorcerer" {
+		charisma := rolledScores[len(rolledScores)-1]
+		constitution := rolledScores[len(rolledScores)-2]
+		intelligence := rolledScores[len(rolledScores)-3]
+		dexterity := rolledScores[len(rolledScores)-4]
+		wisdom := rolledScores[len(rolledScores)-5]
+		strength := rolledScores[len(rolledScores)-6]
+		abilityScores := []int{strength, dexterity, constitution, intelligence, wisdom, charisma}
+		return abilityScores
+
+	} else if charClass == "Warlock" {
+		charisma := rolledScores[len(rolledScores)-1]
+		constitution := rolledScores[len(rolledScores)-2]
+		dexterity := rolledScores[len(rolledScores)-3]
+		strength := rolledScores[len(rolledScores)-4]
+		intelligence := rolledScores[len(rolledScores)-5]
+		wisdom := rolledScores[len(rolledScores)-6]
+		abilityScores := []int{strength, dexterity, constitution, intelligence, wisdom, charisma}
+		return abilityScores
+
+	} else if charClass == "Wizard" {
+		intelligence := rolledScores[len(rolledScores)-1]
+		dexterity := rolledScores[len(rolledScores)-2]
+		charisma := rolledScores[len(rolledScores)-3]
+		constitution := rolledScores[len(rolledScores)-4]
+		wisdom := rolledScores[len(rolledScores)-5]
+		strength := rolledScores[len(rolledScores)-6]
+		abilityScores := []int{strength, dexterity, constitution, intelligence, wisdom, charisma}
+		return abilityScores
+
+	}
+	//if (charRace == "Dwarf") {
+
+	//}
+	//abilityScores := []int{strength, dexterity, constitution, intelligence, wisdom, charisma}
 	return abilityScores
 }
 
 func main() {
-	abilityScores := generateAbilityScores()
-	sort.Ints(abilityScores)
-	fmt.Println(abilityScores)
+	scores := finalizeScores()
+	fmt.Printf("Strength: %v\n", scores[0])
+	fmt.Printf("Dexterity: %v\n", scores[1])
+	fmt.Printf("Constitution: %v\n", scores[2])
+	fmt.Printf("Intelligence: %v\n", scores[3])
+	fmt.Printf("Wisdom: %v\n", scores[4])
+	fmt.Printf("Charisma: %v\n", scores[5])
 }
