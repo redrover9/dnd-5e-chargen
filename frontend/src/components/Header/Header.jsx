3130f608d6d840cc28af69b396f586d4c5290500
Grace <grace.thompson97@gmail.com>
2023-06-03 20:35:18 -0700

    add files for frontend

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Header.scss";
+
+const Header = () => (
+  <div className="header">
+    <h2>DnD Character Creator</h2>
+  </div>
+);
+
+export default Header;
